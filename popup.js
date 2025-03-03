ae3a1ac0ed9b3d1d4ba17407ae3a2704ddcfc58f
Rafael Arcanjo <88547716+rafarcanjoatos@users.noreply.github.com>
2025-03-03 13:32:52 -0300

    OKAY

--- popup.js
+++ popup.js
@@ -1,53 +1,67 @@
-// Listen when popup is open
+// Garante que o Pop Up foi aberto
 document.addEventListener('DOMContentLoaded', () => {
   const formLogin = document.getElementById('login-form');
   const loginDiv = document.getElementById('login-div');
   const contentDiv = document.getElementById('content-div');
   const timerPrincipal = document.getElementById('timerPrincipal');
 
+  // Retorna o timer do backend para apresentar em tela
   getTimerBackend().then(timer => { 
     timer = parseInt(timer, 10);
     timer = timer / 60000;
     timerPrincipal.value = timer;
   });
   
+  // Pega o status do agente para definir se irá mostrar tela de login ou a aplicação
   getFinesseStatusFront().then(result => {
     if (result) {
       showDiv(contentDiv);
       hideDiv(loginDiv);
-      logedIn(result);
+      agentStatus(result);
 
     } else {
       showDiv(loginDiv);
       hideDiv(contentDiv);
 
-      // Listen form button is pressed, to save new credentials
+      // Validação de Formulário
       formLogin.addEventListener('submit', (event) => {
+        alert("click");
         event.preventDefault();
         const username = document.getElementById('username').value;
         const password = document.getElementById('password').value;
         const agentId = document.getElementById('agentId').value;
 
         if (username && password && agentId) {
-          chrome.runtime.sendMessage({
-            action: 'saveCredentials',
-            data: {
-              username: username,
-              password: password,
-              agentId: agentId
-            }
-          }, function (response) {
-            if (response.success) {
-              alert("Credenciais salvas com sucesso!");
-              getFinesseStatusFront();
+          try{
+            chrome.runtime.sendMessage({
+              action: 'saveCredentials',
+              data: {
+                username: username,
+                password: password,
+                agentId: agentId
+              }
+            }, 
+            
+            function (response,username,password,agentId) {              
+              if (response.success) {
+                log.dir(response);
+                console.log(response.success);
+                alert("Credenciais salvas com sucesso!");
+                getFinesseStatusFront();
 
-            } else {
-              alert("Falha ao salvar as credenciais.");
-            }
-          });
-
+              } else {
+                loginFormDataRecover(username,password,agentId);
+                throw(error);
+              }
+            });          
+          }
+          catch (error){
+            alert("Falha ao conectar no Finesse" + error);            
+            loginFormDataRecover(username,password,agentId);
+          }
         } else {
           alert("Preencha todos os campos");
+          loginFormDataRecover(username,password,agentId);
         }
       });
     }
@@ -55,7 +69,7 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
-//Listen form button is pressed, to save new credentials
+// Função para Salvar o Timer
 const formTimer = document.getElementById('timer-form');
 formTimer.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -77,21 +91,24 @@ formTimer.addEventListener('submit', (event) => {
       } else {
         console.log(response);
         alert("Falha ao salvar o timer.");
-      }
+    }
     });
   } else {
     alert("Preencha todos os campos");
   }
 });
 
+// Função Assincrona que verifica a conexão com o Finesse no Backend
 async function getFinesseStatusFront() {
   return await connectApiFinesse();
 }
 
+// Função Assincrona que busca o timer no Backend
 async function getTimerBackend() {
   return await getTimer();
 }
 
+// Função recursiva para mostrar Div
 function showDiv(showDiv) {
   if (showDiv) {
     showDiv.classList.remove('hidden');
@@ -100,6 +117,7 @@ function showDiv(showDiv) {
   }
 }
 
+// Função Recursiva para esconder div
 function hideDiv(hideDiv) {
   if (hideDiv) {
     hideDiv.classList.add('hidden');
@@ -108,7 +126,16 @@ function hideDiv(hideDiv) {
   }
 }
 
-function logedIn(finesse) {
+// Função que recupera Dados do formulário de login
+function loginFormDataRecover(username,password,agentId){
+  document.getElementById('username').value = username;
+  document.getElementById('password').value = password;
+  document.getElementById('agentId').value = agentId;
+}
+
+
+// Função que lê objeto do finesse e retorna status do agente
+function agentStatus(finesse) {
   var statusDiv = document.getElementById('status-div');
   var agentNameDiv = document.getElementById('agent-name-div');
   var reasonDiv = document.getElementById('reason-div');
@@ -138,25 +165,8 @@ function logout() {
   const loginDiv = document.getElementById('login-div');
   const contentDiv = document.getElementById('content-div');
 
-  // Clear credentials
+  // Limpa Credenciais
   removeCredentials();
   showDiv(loginDiv);
   hideDiv(contentDiv);
 }
-
-
-// function colorCircleStatus(status) {
-//   const applicationOpenDiv = document.getElementById('application-open-div');
-//   const applicationClosedDiv = document.getElementById('application-closed-div');
-
-//   if (status == true) {
-//     applicationOpenDiv.classList.remove('hidden');
-//     applicationClosedDiv.classList.add('hidden');
-//   } else if (status == false) {
-//     applicationClosedDiv.classList.remove('hidden');
-//     applicationOpenDiv.classList.add('hidden');
-//   } else {
-//     applicationClosedDiv.classList.add('hidden');
-//     applicationOpenDiv.classList.add('hidden');
-//   }
-// }
